b8d3b63662e509a8ae084d6b53c7c3ea56873acd
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2023-01-14 11:42:14 +0200

    Added search results and cards to main page

--- src/app/components/MainPage/MainPage.tsx
+++ src/app/components/MainPage/MainPage.tsx
@@ -1,7 +1,10 @@
-import { Box } from '@mui/system'
 import React from 'react'
+import { Box } from '@mui/system'
+import { News } from '../../../types'
 import { useGetTrendingNewsQuery } from '../../store/services/newsApi'
+import { Results } from './Results'
 import { Search } from './Search'
+import { Card } from '../Card/Card'
 
 export const MainPage: React.FC = () => {
   const { data, isLoading } = useGetTrendingNewsQuery({ country: 'us' })
@@ -14,6 +17,13 @@ export const MainPage: React.FC = () => {
   return (
     <Box sx={{ paddingY: '50px', paddingX: '75px' }}>
       <Search />
+      {!isLoading && data ? <Results newsList={data} /> : ''}
+
+      <Box sx={{ display: 'flex', gap: '45px', flexWrap: 'wrap', justifyContent: 'center' }}>
+        {!isLoading && data
+          ? data.articles.map((News: News, key = 0) => <Card newsData={News} key={key} />)
+          : ''}
+      </Box>
     </Box>
   )
 }
